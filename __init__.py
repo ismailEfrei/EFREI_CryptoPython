cc22383572e8e6d2cb9a11d5a092c188ad91ff67
ismailEfrei <ismail.faddoul@efrei.net>
2025-03-21 10:21:12 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
                                                                                                                                        
 @app.route('/')
 def hello_world():
-    return render_template('hello.html') #COMMENTAIRE
+    return render_template('hello.html') #COMMENTAIRES
 
 key = Fernet.generate_key()
 f = Fernet(key)
